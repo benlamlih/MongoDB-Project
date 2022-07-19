c41491fdb65aee4f3aa4ff78bb04673cf308ee70
benlamlih <mohammed.benlamlih.etu@univ-lille.fr>
2022-07-19 10:16:28 +0100

    add product controller

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -1,19 +1,29 @@
-//const Posts = require('../model/product.model');
+const Product = require('../model/product.model');
 
 
 /**
  * Récupère tous les produits 
  */
-exports.getAll = (req, res) => {
-
+exports.getAll = async (req, res) => {
+    let product = await Product.find();
+    if (product){
+        res.status(200).json({product});
+    } else {
+        res.status(404).json({message: "No products found"});
+    }
 };
 
 /**
  * Récupère un produit par son id
  * @param {string} req.params.id id du produit à récupérer
  */
-exports.get = (req, res) => {
-
+exports.get = async (req, res) => {
+    let product = await Product.findOne({_id: req.params.id});
+    if (product){
+        res.status(200).json({product});
+    }else{
+        res.status(404).json({message: "Product not found"});
+    }
 };
 
 /**
@@ -21,7 +31,14 @@ exports.get = (req, res) => {
  * @param {string} req.body informations du produit à créer
  */
 exports.create = (req, res) => {
-
+    let product = new Product();
+    product.name = req.body.name;
+    product.description = req.body.description;
+    product.price = req.body.price;
+    product.stock = req.body.stock;
+    product.categoryId = req.body.categoryId;
+    product.save();
+    res.status(200).json({product});
 };
 
 /**
@@ -29,15 +46,32 @@ exports.create = (req, res) => {
  * @param {string} req.params.id id du produit à modifier
  * @param {string} req.body informations du produit à modifier
  */
-exports.update = (req, res, next) => {
-
-
+exports.update = async(req, res, next) => {
+    let product = await Product.findOne({_id: req.params.id});
+    if (product){
+        product.name = req.body.name;
+        product.description = req.body.description;
+        product.price = req.body.price;
+        product.stock = req.body.stock;
+        product.categoryId = req.body.categoryId;
+        product.save();
+        res.status(200).json({message: "Product updated"});
+    }else{
+        res.status(404).json({message: "Cannot update, product not found"});
+    }
 };
 
 /**
  * Supprime un produit par son id
  * @param {string} req.params.id id du produit à supprimer
  */
-exports.delete = (req, res, next) => {
+exports.delete = async (req, res, next) => {
 
+    let product = await Product.findOne({ _id: req.params.id });
+    if (product) {
+        product.delete();
+        res.status(200).json({ message: "Product deleted" });
+    } else {
+        res.status(404).json({ message: "Product not found" });
+    }
 };
